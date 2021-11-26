f2971cd90aa7bd8c74de232983b56ee1e01470a7
Monsotya <sonya20033009@gmail.com>
2021-11-26 21:23:15 +0200

    Added classes CreateSugoku and MixSudoku

--- Sudoku.Application/CreateSudoku.cs
+++ Sudoku.Application/CreateSudoku.cs
@@ -4,6 +4,13 @@ namespace Sudoku.Application
 {
     public class CreateSudoku
     {
+        public CreateSudoku(int size, int complexity)
+        {
+        }
+
+        public void DeleteCells()
+        {
+        }
     }
 
 }
---
+++ Sudoku.Application/MixSudoku.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sudoku.Application
+{
+    public class MixSudoku
+    {
+        public void Transpose(int[][] matrix)
+        {
+        }
+		public void SwapRows(int[][] matrix)
+		{
+		}
+        public void SwapColumns(int[][] matrix)
+        {
+        }
+        public void SwapRowAreas(int[][] matrix)
+        {
+        }
+        public void SwapColumnAreas(int[][] matrix)
+        {
+        }
+    }
+}
